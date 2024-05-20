26f8fb41960b80650b795010260f35bc0c750420
emamjion <emamjion@gmail.com>
2024-05-20 11:48:09 +0600

    supplies details page added

---
+++ src/pages/Home/SupplyDetailsPage.tsx
@@ -0,0 +1,41 @@
+import { useLoaderData } from "react-router-dom";
+
+const SupplyDetailsPage = () => {
+    const details = useLoaderData();
+    const { title, _id, image, category, quantity , description1, description2, description3  } = details;
+    
+    return (
+        <div className="my-16 px-4 md:px-0 ">
+            <div className="flex items-center justify-center h-screen">
+                <div className="text-center">
+                    <img src={image} className="mx-auto" />
+                    <div>
+                        <h1 className="text-3xl font-semibold my-6">{title}</h1>
+                        <p className="text-lg w-[600px] mb-3">
+                            {description1}
+                        </p>
+                        <p className="text-lg w-[600px] mb-3">
+                            {description2}
+                        </p>
+                        <p className="text-lg w-[600px] mb-3">
+                            {description3}
+                        </p>
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center justify-center">
+                                <p className="text-lg font-medium">Category :</p>
+                                <span className="text-lg font-semibold ml-2">{category}</span>
+                            </div>
+                            <div className="flex items-center justify-center">
+                            <p className="text-lg font-medium">Quantity :</p>
+                                <span className="text-lg font-semibold ml-2">{quantity}</span>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SupplyDetailsPage;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home/Home/Home";
 import SuppliesPage from "../pages/Home/SuppliesPage";
+import SupplyDetailsPage from "../pages/Home/SupplyDetailsPage";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,11 @@ const router = createBrowserRouter([
             {
                 path: '/supplies',
                 element: <SuppliesPage/>
+            },
+            {
+                path: '/supplies/:id',
+                element: <SupplyDetailsPage/>,
+                loader: ({params}) => fetch(`http://localhost:5000/supplies/${params.id}`)
             }
         ]
     }
